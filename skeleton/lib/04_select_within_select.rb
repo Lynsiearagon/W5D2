5b7425cf796be2f3574f49e7a65afca26db441f9
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-06 17:42:49 -0500

    working through sparse_continents, still have thoughts - moving into flex time.

--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -121,5 +121,15 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+
+
+
+      SELECT
+        name, continent, population
+      FROM
+        countries
+      WHERE
+        population < 25000000;
+
   SQL
 end
